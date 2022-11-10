7e1526ba0c9220f1a56ac9737a9e5ef8b726a095
olmytro <ironbro24@gmail.com>
2022-11-10 20:02:44 +0300

    class done
    add method

--- test oop 3 - inheritance/Casino.cs
+++ test oop 3 - inheritance/Casino.cs
@@ -6,5 +6,58 @@ namespace test_oop_3___inheritance
 {
     class Casino
     {
+        public int Choose;
+
+        public void EasyFor()
+        {
+            Choose = 5;
+            Random rnd = new Random();
+            int secret = rnd.Next(0, 10);
+
+            Console.WriteLine("Write your number: ");
+
+            for (int i = 0; i < Choose; i++)
+            {
+                int myNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (myNumber == secret)
+                {
+                    Console.WriteLine("You win.");
+                    Console.WriteLine($"Result was: {secret}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Wrong number. Write again. Осталось попыток:");
+                    Console.WriteLine(Choose - i - 1); // но i начинается с 0, нужно сделать чтобы отнимался i после коннактации.
+                }
+            }
+        }
+        
+        public void EasyDo()
+        {
+            Choose = 5;
+            Random rnd = new Random();
+            int secret = rnd.Next(0, 10);
+            do
+            {
+                Console.WriteLine("Write your number: ");
+                int myNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (myNumber == secret)
+                {
+                    Console.WriteLine("You win.");
+                    Console.WriteLine($"Result was: {secret}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Wrong number. Write again. Осталось попыток:");
+                    //Console.WriteLine(Choose - i -1);
+                }
+
+            }
+            while (Choose > 0);
+        }
     }
 }
--- test oop 3 - inheritance/Program.cs
+++ test oop 3 - inheritance/Program.cs
@@ -6,68 +6,25 @@ namespace test_oop_3___inheritance
     {
         static void Main(string[] args)
         {
+            Casino cas = new Casino();
             Console.WriteLine("Select option: 1- easy, 2- medium, 3- hard");
-            string choice= Console.ReadLine();
+            string choice = Console.ReadLine();
             switch (choice)
             {
                 case "1":
                     Console.WriteLine("You chose an easy difficult");
-                    int Try = 5;
+                    //cas.EasyDo();
+                    cas.EasyFor();
                     break;
                 case "2":
                     Console.WriteLine("You chose a medium difficult");
-                    int Try = 3;
+                   // int Try = 3;
                     break;
                 case "3":
                     Console.WriteLine("You chose a hard difficult");
-                    int Try = 1;
+                    //int Try = 1;
                     break;
             }
-            
-            
-
-            Random rnd = new Random();
-            int secret = rnd.Next(0, 10);
-            
-            Console.WriteLine("Write your number: ");
-            
-            for (int i = 0; i < Try; i++) 
-            {
-                int myNumber = Convert.ToInt32(Console.ReadLine());
-
-                if (myNumber == secret)
-                {
-                    Console.WriteLine("You win.");
-                    Console.WriteLine($"Result was: {secret}");
-                    break;
-                }
-                else 
-                {
-                    Console.WriteLine($"Wrong number. Write again. Осталось попыток:");
-                    Console.WriteLine(Try - i); // но i начинается с 0, нужно сделать чтобы отнимался i после коннактации.
-                }
-            }
-            
-            /*do
-            {
-                Console.Write("write number: ");
-                int numb = Convert.ToInt32(Console.ReadLine());
-
-                if (numb == i)
-                {
-                    Console.WriteLine("You win.");
-                    Console.WriteLine($"Result was: {i}");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("You are lost. Write again");
-                    Count--;
-                }
-
-            }
-            while (Count > 0);*/
-
 
             Console.ReadKey();
 
